23b094f985aa4bb2ee119960a659b1b39f3f8d69
mschoettner <mikkel.schoettner@yahoo.de>
2020-07-20 11:31:40 +0200

    Added bids_script.py Created script to make folder structure and copy files

---
+++ bids_script.py
@@ -0,0 +1,24 @@
+def run(data, anat = True, func = True, fieldmap = True):
+    """
+    Creates folders and copies files according to BIDS.
+    Arguments:
+        data: path to where the data are stored. The script expects all subfol-
+        ders to be subjects with respective folders for the given modalities.
+    """
+    anatfolder = input("Where is the anatomical data stored?")#texteingabe
+    funcfolder = #texteingabe
+    fieldmapfolder = #texteingabe
+    subjects = #count folders in data folder
+    for sub in subjects:
+        anatfun()
+        funcfun()
+        fieldmapfun()
+
+def anatfun(arg):
+    pass
+
+def funcfun():
+    pass
+
+def fieldmapfun(arg):
+    pass
